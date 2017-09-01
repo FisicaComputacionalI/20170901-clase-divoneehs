8a8a11cb3fef08fb6c2f8fe1b936560e251d5152
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-09-01 13:11:16 -0500

    Add files via upload
    Esta es la segunda grafica de la clase, la de las mascotas

--- mascotas.py
+++ mascotas.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pylab as pl
-x=[1,2,3,4,5]
-y=[1,4,9,16,25]
-pl.plot(x,y)
-pl.savefig('grafica1sep.png')
+x = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+y = [2   ,2   ,2   ,1   ,1   ,1   ,1   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,1   ,1   ,3   ,3   ,2   ,0   ,0]
+pl.plot(x, y)
+pl.savefig('graficamascotas.png')
